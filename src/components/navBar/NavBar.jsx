b06680b43335a5edc8533c9d29ed06fa77048310
phatommobb <nickoluoch1130@hgmail.com>
2024-05-16 19:14:24 +0300

    home page

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,28 @@
 import './navBar.scss'
+import { Link } from "react-router-dom";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+        <div className="left">
+            <Link to="/Home" style={{textDecoration:"none"}}>
+              <span>SafeHaven Connect</span>
+              <HomeOutlinedIcon/>
+              <DarkModeOutlinedIcon/>
+              <GridViewOutlinedIcon/>
+            </Link>   
+        </div>
+        <div className="right"></div>
+    </div>
   )
 }
 
