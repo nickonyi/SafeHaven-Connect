7390ab57c5b1bacc3d3a2766030afb221bd826f8
phatommobb <nickoluoch1130@hgmail.com>
2024-05-16 05:51:26 -0400

    login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,24 @@ import './login.scss'
 
 function Login() {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <div className="card">
+        <div className="left">
+          <h1>SafeHaven Connect</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste earum, numquam, dignissimos, consectetur aspernatur aliquid debitis itaque quod consequatur at quia tenetur quis ipsam! Aut dolorem odio numquam hic magni!</p>
+           <span>Don't you have an account?</span>
+           <button>Register</button>
+        </div>
+        <div className="right">
+           <h1>Login</h1>
+            <form>
+              <input type="username" placeholder='username' />
+              <input type="password" placeholder='Password' />
+              <button>Login</button>
+              </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
