a2c291a66088d0f97c39f9a94557351ecec19601
unknown <nickoluoch1130@gmail.com>
2024-05-19 21:43:32 +0300

    left bar

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -2,7 +2,16 @@ import './leftBar.scss'
 
 function LeftBar() {
   return (
-    <div>LeftBar</div>
+    <div className='leftbar'>
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+           {/*<img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />*/}
+            <span>John Wafula</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
