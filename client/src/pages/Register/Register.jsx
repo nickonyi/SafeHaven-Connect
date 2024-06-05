6a33e158c71e2259ac82feebd4278973a73388fa
unknown <nickoluoch1130@gmail.com>
2024-06-05 08:31:40 +0300

    more code

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -35,7 +35,7 @@ function Register() {
       <div className="card">
         <div className="left">
           <h1>SafeHaven Connect</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste earum, numquam, dignissimos, consectetur aspernatur aliquid debitis itaque quod consequatur at quia tenetur quis ipsam! Aut dolorem odio numquam hic magni!</p>
+          <p>Lorem ipsum dolor sit, awmet consectetur adipisicing elit. Iste earum, numquam, dignissimos, consectetur aspernatur aliquid debitis itaque quod consequatur at quia tenetur quis ipsam! Aut dolorem odio numquam hic magni!</p>
            <span>Do you have an account?</span>
            <Link to="/Login">
            <button>Login</button>
