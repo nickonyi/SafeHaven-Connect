a2b2ddd644d53f42003a6cc7573542514f6e802f
phatommobb <nickoluoch1130@gmail.com>
2024-06-07 15:31:09 +0300

    feat: Add API endpoint to add a comment

--- api/controller/comment.js
+++ api/controller/comment.js
@@ -14,3 +14,29 @@ export const getComments = (req,res)=> {
             return res.status(200).json(data);
         })
 }
+
+export const addComment =  (req, res) => {
+    const token = req.cookies.accessToken;
+    console.log(token);
+    if(!token) return res.status(401).json('Not logged in!');
+    jwt.verify(token,"secretkey",(err,userInfo)=> {
+        if(err) return res.status(403).json('Invalid token!');
+
+        const sql = "INSERT INTO comments (`desc`,`createdAt`,`userId`,`postId`) VALUES (?)";
+
+        const values = [
+           req.body.desc,
+           moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'),  
+           userInfo.id,
+           req.body.postId 
+        ]
+    
+        db.query(sql,[values],(err,data)=>{
+            if(err){
+                return res.status(500).json(err);
+            }
+    
+            return res.status(200).json("Commented added!");
+        })
+    })
+};
--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {getComments } from "../controller/comment.js";
+import {addComment } from "../controller/comment.js";
 
 const router = express.Router();
 router.get("/",getComments);
+router.post("/",addComment);
 
 export default router;
