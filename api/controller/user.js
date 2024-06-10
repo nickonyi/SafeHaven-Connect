36e43f567cdc98a8a689423914d1fbe1aa55bc88
phatommobb <nickoluoch1130@gmail.com>
2024-06-10 18:53:23 +0300

    feat: Retrieve user information from the database in getUser API endpoint

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,3 +1,16 @@
-export const getUser = (req, res) => {
+import { db } from "./connect.js";
+import jwt from "jsonwebtoken";
 
+export const getUser = (req, res) => {
+ const userId = req.params.userId;
+
+    const sql = `SELECT * FROM users WHERE id = ?`;
+
+    db.query(sql, [userId], (err, data) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+         const {password,confirmPassword, ...info} = data[0];
+        return res.status(200).json(info);
+    })
 }
