09278450585cbe562e521c72ec3a49929326aeb7
phatommobb <nickoluoch1130@gmail.com>
2024-06-17 11:58:10 +0300

    right bar component

--- client/src/components/rightBar/RightBar.jsx
+++ client/src/components/rightBar/RightBar.jsx
@@ -39,43 +39,6 @@ console.log(data);
           )
         )
         }
-          
-        <div className="item">
-           <span>Latest activities</span>
-           <div className="user">
-            <div className="user-info">
-              <img src={currentUser.profilePic} alt="" />
-              <p>
-              <span>{currentUser.name} </span> changed their cover picture
-              </p>
-            </div>
-            <div className="buttons">
-              <span>1 min ago</span>
-            </div>
-          </div>
-          <div className="user">
-            <div className="user-info">
-              <img src={currentUser.profilePic} alt="" />
-              <p>
-              <span>Jane Doe  </span> changed their profile picture
-              </p>
-            </div>
-            <div className="buttons">
-              <span>1 min ago</span>
-            </div>
-          </div>
-          <div className="user">
-            <div className="user-info">
-              <img src={currentUser.profilePic} alt="" />
-              <p>
-              <span>Jane Doe  </span> just came online
-              </p>
-            </div>
-            <div className="buttons">
-              <span>1 min ago</span>
-            </div>
-          </div>
-        </div>
         <div className="item">
           <span>Online friends</span>
           <div className="user">
