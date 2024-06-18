468776ec80dbcfafb1a127abfe06b78e41a8fab2
unknown <nickoluoch1130@gmail.com>
2024-06-18 22:09:17 +0300

    Messenger page

---
+++ client/src/pages/messenger/messenger.jsx
@@ -0,0 +1,16 @@
+import './messenger.scss'
+
+
+
+function Messenger() {
+
+    
+  return (
+    <>
+        <div className='Messenger'>messenger</div>
+    </>
+    
+  )
+}
+
+export default Messenger
