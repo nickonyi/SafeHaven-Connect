3b7c268a410bd01214c157df59dc8b9d298f7fc9
phatommobb <nickoluoch1130@gmail.com>
2024-07-02 14:24:06 +0300

    feat: Update EventNavBar component with improved navigation links

--- client/src/components/eventNavBar/EventNavBar.jsx
+++ client/src/components/eventNavBar/EventNavBar.jsx
@@ -1,8 +1,21 @@
 import './EventNavBar.scss'
+import { Link, useLocation } from 'react-router-dom';
 
 function EventNavBar() {
   return (
-    <div>EventNavBar</div>
+    <div className="header">
+        <div className="nav">
+            <div>
+            <Link to="/" style={{textDecoration:"none"}}>
+              <span>SafeHaven Connect</span>
+              </Link> 
+               <ul>
+               <Link className="s" to="/Events/horizontal/"><li>HOME</li></Link>
+               <Link className="s" to="/Events/horizontal/event"><li>EVENT</li></Link>
+                </ul> 
+            </div>
+        </div>
+    </div>
   )
 }
 
