082d40f1eee9e137c708cf8870764a80e9e2e8f1
phatommobb <nickoluoch1130@gmail.com>
2024-07-03 12:53:28 +0300

    event home page

--- client/src/components/eventHomePage/EventHomePage.jsx
+++ client/src/components/eventHomePage/EventHomePage.jsx
@@ -1,14 +1,45 @@
+import { useState } from 'react'
 import './EventHomePage.scss'
 
 function EventHomePage() {
+    const [loading,isLoading] = useState(false);
+
+    const handleSearchButtonClick = ()=> {
+
+    }
+
   return (
     <div className='event-home-page'>
-        <div className="background-image">
             <h2>Walking street under the tunnel</h2>
             <h3>Get Ready For The Next Event</h3>
             <button className='home-btn'>Buy Ticket</button>
+
+            <div className="search">
+                <input 
+                type="text" 
+                name ="text" 
+                id ="text"
+                placeholder='Enter a name...' 
+                />
+
+                <select name="" id="" className="homepage-select">
+                    <option value="today">Today</option>
+                    <option value="tomorrow">Tomorrow</option>
+                    <option value="this_week">This Week</option>
+                    <option value="next_week">Next Week</option>
+                    <option value="next_month">Next Month</option>
+                    <option value="future">Future</option>
+                </select>
+                <button className='homepage-button' onClick={handleSearchButtonClick}>
+                    {loading ? (
+                      <span>Loading...</span>
+                    ) : (
+                      <span>Search</span>
+                    )}
+                </button>
+            </div>
         </div>
-    </div>
+   
   )
 }
 
