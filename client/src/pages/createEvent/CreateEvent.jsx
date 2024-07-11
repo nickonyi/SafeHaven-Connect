2e781e9e78b46227109b70fc6c951785c20a1afe
phatommobb <nickoluoch1130@gmail.com>
2024-07-11 11:24:28 +0300

    refactor: Remove unused routes and components related to events

--- client/src/pages/createEvent/CreateEvent.jsx
+++ client/src/pages/createEvent/CreateEvent.jsx
@@ -2,11 +2,13 @@ import './CreateEvent.scss'
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from 'react';
 
+
 function CreateEvent() {
   const {currentUser} = useContext(AuthContext);
   
   return (
     <div>
+      
       { currentUser && <div>CreateEvent</div>}
     </div>
   )
