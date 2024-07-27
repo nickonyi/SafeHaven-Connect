63e1b239c1f3cfc762808fdbd0ce2f3078d00032
phatommobb <nickoluoch1130@gmail.com>
2024-07-27 14:33:55 +0300

    feat: Add FontAwesome icons and update event details page styling

--- client/src/pages/eventDetailsPage/EventDetailsPage.jsx
+++ client/src/pages/eventDetailsPage/EventDetailsPage.jsx
@@ -3,6 +3,8 @@ import './EventDetailsPage.scss'
 import Static from '../../components/static/Static';
 import { EventContext } from '../../context/EventContext';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 function EventDetailsPage() {
@@ -28,8 +30,16 @@ function EventDetailsPage() {
     }
    }
 
+   const formatDate = (dateString) => {
+    const options = { month: 'short', day: '2-digit', year: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', options);
+   }
+
+
+
   return (
-    <div>
+    <div className='event-detales'>
         {
             eventDetails?
             <div>
@@ -37,9 +47,63 @@ function EventDetailsPage() {
              <div className="detailsevent">
                <div className="eventdatailpimg shadow-md">
                 <img src={eventDetails.image} alt="" />
+                <div>
+                    {
+                        eventDetails.description
+                    }
+                </div>
                </div>
+               <div className="locationandmap shadow-md">
+                <div className="locationandmaptop">
+                  <div className="locationandmaptilte">
+                    <h3>EVENT DETAIL</h3>
+                    <FontAwesomeIcon icon={faInfoCircle} className="event-detail-icon" />
+                  </div>
+                  <div className="locationandmapflex">
+                    <h4>Date:</h4>
+                    <p>{formatDate(eventDetails.date)}</p>
+                  </div>
+                  <div className="locationandmapflex">
+                  <h4>Venue:</h4>
+                  <p>{eventDetails.venue}</p>
+                  </div>
+                  <div className="locationandmapflex">
+                    <h4>Location:</h4>
+                    <p>{eventDetails.location}</p>
+                  </div>
+               </div>
+              </div>   
              </div>
+
+            <div className="ticketdisplay">
+              <h4 className='ticketdisplayh4'>TICKET&PRICE</h4>
+              <div className="ticketshow">
+              {Ticket && Ticket.length > 0 ? 
+              (
+                <div className="ticketshow">
+                  {Ticket.map((ticket) => (
+                    <div className="ticketbody" key={ticket._id}>
+                      <button className="ticketbodybtn">{ticket.type}</button>
+                      <img src="/price_icon.png" alt="" className="ticketbodyImg" />
+                      <h4 className="ticketbodyh4">{ticket.price === 0 ? "Free" : `₦${ticket.price}`}</h4>
+                      <p className="ticketbodyp">{ticket.sit} Tickets</p>
+                      <button
+                        className="registerforevent"
+                        onClick={() => handleRegisterForEvent(ticket._id, ticket.type)}
+                      >
+                        REGISTER EVENT
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              ):
+              ( <p>No tickets available</p>)
+              }
+              </div>
+              </div>
             </div> 
+
+            
              :
               (
                 <div className="d-flex justify-content-center align-items-center" style={{ paddingTop: '150px', paddingBottom: '50px' }}>
